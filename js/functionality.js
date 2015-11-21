5211f659fa2496e405bad9160b034e7738b5f158
Alex Ly <alexly@nbp-234-87.nbp.ruw.rutgers.edu>
2015-11-21 10:12:04 -0500

    working add, need to pass value from input

--- js/functionality.js
+++ js/functionality.js
@@ -1,12 +1,16 @@
 // Add row
 
 $('#add-button').click(function() {
-  var newRow = "<tr></tr>";
-  var td = "<td></td>";
+  var newRow = $(".to-do-list tbody");
+  var tr = $('<tr></tr>');
+  var checkTd = $('<td class="col-md-1 col-lg-1 text-center"><input class="check" type="checkbox"></td>');
+  var itemTd = $('<td class="col-md-10 col-lg-10 item-description"></td>');
+  var delTd = $('<td class="col-md-1 col-lg-1 text-right del-button-td"><button class="btn btn-default del-button">X</button></td>');
 
-  $('table.to-do-list').prepend(newRow);
-
-  
+  tr.append(checkTd);
+  tr.append(itemTd);
+  tr.append(delTd);
+  newRow.append(tr);
 });
 
 // Delete blinker and delete row
